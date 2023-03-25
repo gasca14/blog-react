586466e725ce5eec584f84770ad408c8dc92cb3b
gasca14 <alexgasca89@gmail.com>
2023-03-24 20:58:01 -0600

    cambios
    seccion de contacto

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,9 +1,26 @@
-import PaginaConstruccion from "../../components/PaginaConstruccion/PaginaConstruccion";
-
+// import PaginaConstruccion from "../../components/PaginaConstruccion/PaginaConstruccion";
+import '../Contacto/Contacto.css'
+import ubicacion from '../../assets/images/ubicacion.png'
+import iparbethel from '../../assets/images/iparbethel.png'
 const Contacto = () => {
     return ( 
         <>
-            <PaginaConstruccion></PaginaConstruccion>
+            {/* <PaginaConstruccion></PaginaConstruccion> */}
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="offset-3 col-6">
+                        <p id='direccion'>Dirección: Calle 5 de febrero #16, san isidro, 79386 Cárdenas, S.L.P</p>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-lg-6 col-md-6">
+                        <img id='ubica' src={ubicacion} alt="" />
+                    </div>
+                    <div className="col-lg-6 col-md-6">
+                        <img id='ipar' src={iparbethel} alt="" />
+                    </div>
+                </div>
+            </div>
         </>
      );
 }
